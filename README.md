e2f4bf2c5dbe0815de7b948e54a1a7a2522ac204
Kim David Hauser <kim@kimhauser.ch>
2022-04-30 11:09:28 +0200

    First  commit
    First commit of new project with main Activity and fragments

--- README.md
+++
@@ -1,2 +0,0 @@
-# ch.kimhauser.android.KnApp
-KnApp is an android app mainly for showing which K&amp;A is open at the moment
---
+++ app/src/main/java/ch/kimhauser/android/knapp/ui/home/HomeFragment.kt
@@ -0,0 +1,71 @@
+package ch.kimhauser.android.knapp.ui.home
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import java.text.SimpleDateFormat
+import java.util.*
+import ch.kimhauser.android.knapp.R
+import ch.kimhauser.android.knapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    val simpleDateFormat = SimpleDateFormat("hh:mm:ss")
+    private var txtTime: TextView? = null //= binding.txtTime
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        /*val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })*/
+
+        txtTime = binding.txtTime
+
+        val mainHandler = Handler(Looper.getMainLooper())
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                updateTime()
+                mainHandler.postDelayed(this, 1000)
+            }
+        })
+
+        return root
+    }
+
+    fun updateTime(){
+        if(txtTime != null) {
+            val calendar: Calendar = Calendar.getInstance()
+            val dateTime = simpleDateFormat.format(calendar.time)
+            txtTime!!.text = dateTime
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
